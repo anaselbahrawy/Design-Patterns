45b62dacf2540298a0571a9a8050bf55c861e2c8
Your Name <you@example.com>
2018-02-10 18:28:16 +0200

    add code

--- Singleton.php
+++ Singleton.php
@@ -2,4 +2,27 @@
 /*
 * Design Patterns
 * Singleton Design Patterns
+* Example
 */
+
+class Registry
+{
+
+  private static $instance;         // it use a create new in this class when called namespace global
+
+  private function __construct(){}  //this is meaning not access anywhere this class
+
+  private function __clone(){}     // this is meaning not editing the class, it use change method before called
+
+  public static function getInstance()  //the function check the class exist or no, if not exist it is exist call
+  {
+    if (self::$instance === null) {
+       self::$instance = new self();
+    }
+    return self::$instance;
+  }
+
+}
+var_dump(Registry::getInstance());
+
+//$reg = new Registry(); //  Uncaught Error: Call to private Registry::__construct() , can't call class is construct private
